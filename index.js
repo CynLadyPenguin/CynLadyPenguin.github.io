4ae355c11f1acc647450391079470eeeaa7d72cc
Cynthia <ladypenguin1991@gmail.com>
2022-12-08 17:31:26 -0600

    Twiddler

--- index.js
+++ index.js
@@ -1,39 +1,98 @@
 
 $(document).ready(() => {
+
+  
   const $body = $('body');
   $body.html(''); //clear a tag in this case the body
-
-  const $tweets = streams.home.map((tweet) => {
+  //create a main section in the body to host the central content for the page
+  var $mainContent = $('<main></main>').attr('id', 'main-content');
+  //append it to the body
+  $mainContent.appendTo($body);
+  const $mainTweets = $('<div class="main-tweets"></div>');
+  $mainTweets.appendTo($mainContent);
+  
+  
+  const $tweets = function(){
+    return streams.home.map((tweet) => {
     const $tweet = $('<div></div>'); //creates a div for tweets
     const text = `@${tweet.user}: ${tweet.message}`;
 
-    $tweet.text(text);
-
+    $tweet.text(text + "    " + moment().calendar());
+    $mainTweets.prepend($tweet);
     return $tweet;
   });
-  $body.append($tweets);
-
-
-//create a button to load new tweets
-const buttonNewTweets = $('<section>').attr('id', 'button-new-tweets').addClass('button');
-//append buttonNewTweets to body
-$('body').append(buttonNewTweets);
-//create the button function that makes it clickable 
-  var button = document.createElement('button');
-  button.id = 'button';
-  button.innerHTML = 'Update Tweets';
-  button.className = 'btn-styled';
-//create button function to refresh the randomly generated tweets
-  button.onclick = function() {
-      location.reload(true);
-  };
-
-//add button function to button section at top of body
-$('body').prepend(buttonNewTweets);
-$('#button-new-tweets').prepend(button);
+};
 
 
 
+const $veryTweet = $tweets();
+
+
+
+//these are some variables to help with the creation of new tweets
+const $newTweets = $('<form id="newTweets"></form>');
+const $visitorsName = $('<div><label for="visitorsName">Username</label>\n<input type="text" id="visitorsName"></div>');
+const $visitorsTweet = $('<div><label for="visitorsTweet">Tweet</label>\n<input type="text" id="visitorsTweet"></div>');
+const $visitorsButton = $('<button>Submit</button>');
+
+
+
+const $loadTweetsButton = $('<button>New Tweets</button>').prependTo($mainContent);
+$loadTweetsButton.on('click', function(){
+    $tweets();
+});
+
+$visitorsButton.on('click', function(){
+    const inputUsernameTextbox = $('#visitorsName').val();
+    const inputTweetTextbox = $('#visitorsTweet').val();
+    const userTweet = "@" + inputUsernameTextbox + ': ' + inputTweetTextbox + " " + moment().calendar();
+    $mainTweets.prepend(userTweet);
+})
+
+// inputUsernameTextbox.on('click', function(){
+//   $pageName.text(inputUsernameTextbox + `'s Timeline`);
+//   $mainTweets.html(users.inputUsernameTextbox);
+// });
+
+
+//[maincontent sections creation section:
+
+$newTweets.append($visitorsName, $visitorsTweet, $visitorsButton);
+$newTweets.prependTo($mainContent);
+
+const $pageName = $('<h2>Home Page</h2>');
+$pageName.prependTo($mainContent);
+
+
+//end of maincontents sections creation section]
+
+
+
+//let's make ourselves a header
+var $header = $('<header></header>');
+//we'll give it a name and a size of h1 and a class of header so we can make changes easily later
+$header.text('Welcome To Twiddler').attr('class', 'header');
+//now we'll add it to the body 
+$header.prependTo($body);
+
+
+
+
+//create the newTweets function that displays the visitors tweets on the homepage
+$('#newTweets').submit(function(event) {
+  event.preventDefault();
+  window.visitor = $('#visitorsName').val();
+  writeTweet($('#visitorsTweet').val());
+  $('#visitorsName').val('');
+  $('#visitorsTweet').val('');
+});
+
+
+function displayUsersTimeline(user) {
+  $pageName.text(user + `'s Timeline`);
+  $loadTweetsButton.text(`Let's Go Home`);
+  loadTweets(streams.users[user]);
+}
 
 
 
@@ -45,24 +104,8 @@ $('#button-new-tweets').prepend(button);
 
 
 
+// //this will be the styling section for making it look pretty
 
 
-
-
-
-
-
-
-
-//this will be the styling section for making it look pretty
-/**
- * 
- * 
- * 
- * 
- * 
- * 
- * 
- */
 
 });
