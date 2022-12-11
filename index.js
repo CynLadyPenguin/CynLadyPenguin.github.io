e4faa780fcadaf1b4b265ea6b038c161d088e898
Cynthia <ladypenguin1991@gmail.com>
2022-12-11 10:46:22 -0600

    Twiddler commit

--- index.js
+++ index.js
@@ -8,28 +8,50 @@ $(document).ready(() => {
   var $mainContent = $('<main></main>').attr('id', 'main-content');
   //append it to the body
   $mainContent.appendTo($body);
+  //create a div to hold the tweets separately from the main content so the textboxes and buttons won't be affected
   const $mainTweets = $('<div class="main-tweets"></div>');
+  //append mainTweets to mainContent
   $mainTweets.appendTo($mainContent);
-  
-  
-  const $tweets = function(){
+
+  //we'll create a function
+  const $tweets = function() {
+    //that maps through the home page
     return streams.home.map((tweet) => {
-    const $tweet = $('<div></div>'); //creates a div for tweets
-    const text = `@${tweet.user}: ${tweet.message}`;
-
-    $tweet.text(text + "    " + moment().calendar());
+      //with a new div to hold the home tweets
+    const $tweet = $('<div></div>'); 
+    //and a variable to hold the user name
+    const tweetName = tweet.user;
+    //then give it an id so we can access it later
+    $tweet.attr('id', 'whole-page-tweets');
+    //next we need some text to put inside the tweet
+    const text1 = ('@' + tweet.user + ': ' + tweet.message + " " + moment().calendar());
+    //and to append that text to the div
+    $tweet.append(text1);
+    //create a new line so we can add a friendly timestamp
     $tweet.append('<br>');
+    //then create an i to hold the timestamp
     const $timestamp = $('<i class="timestamp"></i>');
+    //add the infor for the timestamp to the i for timestamp
     $timestamp.append(moment(tweet.created_at).fromNow());
+    //and give the timestamp some title text
     $timestamp.attr('title', moment(tweet.created_at).format('MMM DD, YYYY, h:mm:ss a'));
+    //add the timestamp to the tweet div below the tweet
+    $timestamp.append('<br></br>');
     $timestamp.appendTo($tweet);
+    //then add that to the mainTweets body at the top 
     $mainTweets.prepend($tweet);
-  });
-};
-
-
-
-const $veryTweet = $tweets();
+    //next we need a function that allows us to click on the username
+    $tweet.click(function(){
+      //we'll return a function inside the click function that will change the page how we like
+      const $userTweet = () => {
+        //this function will return displayUsersTimeline because that function does what i want
+       displayUsersTimeline(tweetName);
+    };
+    $mainTweets.append($userTweet);
+    })
+      
+    })
+  };
 
 
 
@@ -43,8 +65,11 @@ const $visitorsButton = $('<button>Submit</button>');
 
 const $loadTweetsButton = $('<button>New Tweets</button>').prependTo($mainContent);
 $loadTweetsButton.on('click', function(){
+  $mainTweets.empty();
     $tweets();
 });
+
+
 
 $visitorsButton.on('click', function(){
     const inputUsernameTextbox = $('#visitorsName').val();
@@ -53,22 +78,19 @@ $visitorsButton.on('click', function(){
     $mainTweets.prepend(userTweet);
 })
 
-// inputUsernameTextbox.on('click', function(){
-//   $pageName.text(inputUsernameTextbox + `'s Timeline`);
-//   $mainTweets.html(users.inputUsernameTextbox);
-// });
 
 
-//[maincontent sections creation section:
-
+//add the forms and button for the visitor to tweet to the newTweets form
 $newTweets.append($visitorsName, $visitorsTweet, $visitorsButton);
+//add all that to the top of mainContent
 $newTweets.prependTo($mainContent);
 
+
+
+//I want to be able to change what the page says 
 const $pageName = $('<h2>Home Page</h2>');
+//add the pageName change ability to the top of mainContent
 $pageName.prependTo($mainContent);
-
-
-//end of maincontents sections creation section]
 
 
 
@@ -81,34 +103,50 @@ $header.prependTo($body);
 
 
 
-
 //create the newTweets function that displays the visitors tweets on the homepage
 $('#newTweets').submit(function(event) {
+  //prevent the default 
   event.preventDefault();
+  //take the visitor's name value from the box
   window.visitor = $('#visitorsName').val();
+  //use the writeTweet function on the value of the textbox for visitor tweet
   writeTweet($('#visitorsTweet').val());
+  //pull value from the name and tweet box
   $('#visitorsName').val('');
   $('#visitorsTweet').val('');
 });
 
 
+
+//a function that changes the aspects on the page
+//will be tied to the user click function
 function displayUsersTimeline(user) {
+  $mainTweets.empty();
+  const $saltyTweet = $('<div id="salty-tweets-by-click"></div>');
+  //map through the user's tweets
+  const mapUser = streams.users[user].map((tweet) => {
+    //another var for the message inside this function
+    const text2 = ('@' + tweet.user + ': ' + tweet.message + " " + moment().calendar());
+      //append the text to the saltyTweet div
+      $saltyTweet.append(text2 + "    ");
+      $saltyTweet.append('<br></br>');
+      //append saltyTweet to the mainTweets
+      $mainTweets.append($saltyTweet);
+      return $mainTweets;
+  })
+  //change the pagename to the user's timeline
   $pageName.text(user + `'s Timeline`);
+  //change the new tweets button to say let's go home
   $loadTweetsButton.text(`Let's Go Home`);
-  loadTweets(streams.users[user]);
+  //when you click on the let's go home button it should show the previous tweets
+  $loadTweetsButton.on('click', function(){
+    $mainTweets.show();
+    //change the new tweets button back
+    $loadTweetsButton.text('New Tweets');
+    //and change back the pagename
+    $pageName.text('Home Page');
+  })
 }
-
-
-
-
-
-
-
-
-
-
-
-// //this will be the styling section for making it look pretty
 
 
 
